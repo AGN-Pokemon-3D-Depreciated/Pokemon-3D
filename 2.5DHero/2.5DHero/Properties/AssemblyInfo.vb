14c9d9673c7992052a50f1f0199ec2b6a9751d6f
jianmingyong <jianming1993@gmail.com>
2017-01-11 02:33:29 +0800

    [Patch] Added files.

---
+++ 2.5DHero/2.5DHero/Properties/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Pokemon 3D")>
+<Assembly: AssemblyDescription("Pokemon 3D")>
+<Assembly: AssemblyCompany("The Dialga Team")>
+<Assembly: AssemblyProduct("Pokemon 3D")>
+<Assembly: AssemblyCopyright("Copyright © Kolben 2012-2015, P3D Team 2016-2017, The Dialga Team 2016-2017")>
+<Assembly: AssemblyTrademark("The Dialga Team")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("2AAF65BE-C81D-4663-ABF4-E2E09B16E12F")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+
+<Assembly: AssemblyVersion("0.54.1.0")>
+<Assembly: AssemblyFileVersion("0.54.1.0")>
